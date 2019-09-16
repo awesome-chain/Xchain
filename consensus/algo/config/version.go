30fddb710a26b0ed7f42df03ea6a445d4d75c40d
qinxiuchen <qinxiuchen@gmail.com>
2019-09-16 18:54:49 +0800

    use xchain

--- consensus/algo/config/version.go
+++ consensus/algo/config/version.go
@@ -1,18 +1,18 @@
 // Copyright (C) 2019 Algorand, Inc.
-// This file is part of go-algorand
+// This file is part of Xchain
 //
-// go-algorand is free software: you can redistribute it and/or modify
+// Xchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// go-algorand is distributed in the hope that it will be useful,
+// Xchain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+// along with Xchain.  If not, see <https://www.gnu.org/licenses/>.
 
 package config
 
@@ -130,5 +130,5 @@ func GetAlgorandVersion() string {
 
 // GetLicenseInfo retrieves the current license information
 func GetLicenseInfo() string {
-	return "go-algorand is licensed with AGPLv3.0\nsource code available at https://github.com/awesome-chain/Xchain/consensus/algo"
+	return "Xchain is licensed with AGPLv3.0\nsource code available at https://github.com/awesome-chain/Xchain/consensus/algo"
 }
--- consensus/algo/data/transactions/keyreg_test.go
+++ consensus/algo/data/transactions/keyreg_test.go
@@ -1,18 +1,18 @@
 // Copyright (C) 2019 Algorand, Inc.
-// This file is part of go-algorand
+// This file is part of Xchain
 //
-// go-algorand is free software: you can redistribute it and/or modify
+// Xchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// go-algorand is distributed in the hope that it will be useful,
+// Xchain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+// along with Xchain.  If not, see <https://www.gnu.org/licenses/>.
 
 package transactions
 
